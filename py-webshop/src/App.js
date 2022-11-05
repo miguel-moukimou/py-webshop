9fe02c17d62754cf5522e48e4c4c3bf4b58c8229
miguel <miguel@cinaq.com>
2022-11-05 18:54:47 +0100

    created a router to link the homepage, products page and the products detail page

--- py-webshop/src/App.js
+++ py-webshop/src/App.js
@@ -1,19 +1,26 @@
-import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header/>
       <main className='py-3'>
         <Container>
-          <h1>Welcome</h1>
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
