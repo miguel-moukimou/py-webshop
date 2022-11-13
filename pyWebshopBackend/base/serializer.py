e25a21c4745d79eff8016e967d02f854236604f8
miguel <miguel@cinaq.com>
2022-11-13 15:15:42 +0100

    configure protected endpoints

--- pyWebshopBackend/base/serializer.py
+++ pyWebshopBackend/base/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import *
 
 
@@ -24,6 +25,17 @@ class UserSerializer(serializers.ModelSerializer):
             name =  userObj.email
         
         return name
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+    
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+
 
 
 class ProductSerializer(serializers.ModelSerializer):
